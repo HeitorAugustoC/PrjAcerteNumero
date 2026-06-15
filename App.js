483ecc2b8610fac276deb0f3fb6a19ab69d5f877
Heitor Augusto <heitor1906@gmail.com>
2026-06-15 10:48:04 -0300

    Ajustando detalhes

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, View } from 'react-native';
 import estilo from './src/style/estilo';
 import { CORES } from './src/style/estilo';
