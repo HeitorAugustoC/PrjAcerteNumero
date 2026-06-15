65c8d133b6db149c475433ff92689538ba9b84fe
Heitor Augusto <heitor1906@gmail.com>
2026-06-15 10:27:10 -0300

    Corrigindo bug de import

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, View } from 'react-native';
 import estilo from './src/style/estilo';
 import { CORES } from './src/style/estilo';
-import TelaJogo from '../PrjAcerteNumero/src/screens/TelaJogo';
+import TelaJogo from './src/screens/TelaJogo';
 
 export default function App() {
   return (
